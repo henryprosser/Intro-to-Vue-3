45dd95186cab5a2e138120606f4f84c45562f786
henryprosser <henryprosser@yahoo.co.uk>
2024-07-22 14:16:06 +0100

    Add attribute binding

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
   data() {
     return {
       product: "Socks",
-      description: "A pair of socks",
+      image: "./assets/images/socks_green.jpg",
+      url: "https://vuejs.org",
     };
   },
 });
